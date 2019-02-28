5c33ceb421b41367f844c067bc5e7f1ec0bfb9a6
gtindo <gtyletindo@gmail.com>
2019-02-28 20:04:29 +0100

    solution naive aux differents cas

--- code_example.js
+++ code_example.js
@@ -14,10 +14,43 @@ let main = fs.readFile('a_example.txt', 'utf8', (err, data) => {
     var photos = [];
     for (let i = 1 ; i <= N; i++){
         let rows = lines[i].split(' ');
-        let photo = new Photo(i, rows[0], rows.slice(2));
+        let photo = new Photo(i-1, rows[0], rows.slice(2));
         photos.push(photo);
     }
     console.log(photos);
+
+    let h_photos = photos.filter(photo => photo.orientation === 'H');
+    let v_photos = photos.filter(photo => photo.orientation === 'V');
+    
+    let s1 = [];
+    h_photos.forEach((photo) => {
+        s1.push(photo.id);
+    })
+
+    let s2 = [];
+    for(let i = 0; i < v_photos.length; i+=2){
+        s2.push([v_photos[i].id, v_photos[i+1].id]);
+    }
+
+    let slides = s1.concat(s2);
+    
+    var solution = slides.length+"\n";
+    
+    slides.forEach((value) => {
+        if(value.length == 2) solution += (value[0] + " "+value[1]+"\n");
+        else solution += (value + "\n");
+    });
+
+    console.log(solution);
+
+    fs.writeFile("solution_example.txt", solution, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    
+        console.log("The file was saved!");
+    }); 
+
 });
 
 
---
+++ code_pet.js
@@ -0,0 +1,56 @@
+let fs = require('fs');
+
+class Photo{
+    constructor(id, orientation, tags){
+        this.id = id;
+        this.orientation = orientation;
+        this.tags = tags;
+    }
+}
+
+let main = fs.readFile('d_pet_pictures.txt', 'utf8', (err, data) => {
+    let lines = data.split('\n');
+    let N = lines[0];
+    var photos = [];
+    for (let i = 1 ; i <= N; i++){
+        let rows = lines[i].split(' ');
+        let photo = new Photo(i-1, rows[0], rows.slice(2));
+        photos.push(photo);
+    }
+    console.log(photos);
+
+    let h_photos = photos.filter(photo => photo.orientation === 'H');
+    let v_photos = photos.filter(photo => photo.orientation === 'V');
+    
+    let s1 = [];
+    h_photos.forEach((photo) => {
+        s1.push(photo.id);
+    })
+
+    let s2 = [];
+    for(let i = 0; i < v_photos.length; i+=2){
+        s2.push([v_photos[i].id, v_photos[i+1].id]);
+    }
+
+    let slides = s1.concat(s2);
+    
+    var solution = slides.length+"\n";
+    
+    slides.forEach((value) => {
+        if(value.length == 2) solution += (value[0] + " "+value[1]+"\n");
+        else solution += (value + "\n");
+    });
+
+    console.log(solution);
+
+    fs.writeFile("solution_pet.txt", solution, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    
+        console.log("The file was saved!");
+    }); 
+
+});
+
+
